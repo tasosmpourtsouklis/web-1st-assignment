5ee11239e16c8c37cb6cc92ab4a346161ac24133
Tasos Mpourtsouklis <tasos.mpourtsouklis@gmail.com>
2022-12-06 20:47:56 +0200

    Implemented error messages based on validation..

--- Exercise5/javascript/validation.js
+++ Exercise5/javascript/validation.js
@@ -1,47 +1,30 @@
-
-// 
-// const email = document.getElementById("email");
-// const phone = document.getElementById("phone");
-// const bDay = document.getElementById("bDay");
-// // Address
-// const region = document.getElementById("region");
-// const county = document.getElementById("county");
-// const postalCode = document.getElementById("postalCode");
-// const address = document.getElementById("address");
-// // Payment
-// const ccNumber = document.getElementById("ccNumber");
-// const ccExpDate = document.getElementById("ccExpDate");
-// const ccCode = document.getElementById("ccCode");
-// const ccName = document.getElementById("ccName");
-// // Account
-// const username = document.getElementById("username");
-// const password = document.getElementById("password");
-// const password1 = document.getElementById("password1");
-// // Approves
-// const approveTerms = document.getElementById("approveTerms");
-
-// // Javascript reGexes for validation
-// const regName = /\d+$/g;
-// const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-// // const regEmail = "[^@\s]+@[^@\s]+\.[^@\s]+"
-// const regPhone = /^\d{10}$/;
-// //const regPostal 
-// //!regName.test(firstName)
-
-function setError(element, message) {
-    window.alert(message);
-    element.focus();
-    return;
-}
-
 function validate() {
     // HTML elements
     // Personal Information
     const firstName = document.getElementById('firstName');
     const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const tel = document.getElementById("tel");
+    const bDay = document.getElementById("bDay");
+    // Address
+    const region = document.getElementById("region");
+    const county = document.getElementById("county");
+    const postalCode = document.getElementById("postalCode");
+    const address = document.getElementById("address");
+    // Payment
+    const ccNumber = document.getElementById("ccNumber");
+    const ccExpDate = document.getElementById("ccExpDate");
+    const ccCode = document.getElementById("ccCode");
+    const ccName = document.getElementById("ccName");
+    // Account
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const password1 = document.getElementById("password1");
+    // Approves
+    const approveTerms = document.getElementById("approveTerms");
 
     // Remove unnecessary whitespaces
-    const firstNameValue = firstName.value.trim();
+    // const firstNameValue = firstName.value.trim();
     const lastNameValue = lastName.value.trim();
     // const emailValue = email.value.trim();
     // const phoneValue = phone.value.trim();
@@ -50,30 +33,67 @@ function validate() {
     // const postalCodeValue = postalCode.value.trim();
 
     // Personal Information
-    if (firstNameValue == "") {
-        setError(firstName, "Please enter your firstName properly.");
-        return false;
+    if (!firstName.checkValidity()) {
+        document.getElementById("firstNameError").innerHTML = "Εισάγετε το όνομα σας.";
+    } else {
+        document.getElementById("firstNameError").innerHTML = "";
     }
 
-    if (lastNameValue == "") {
-        setError(lastName, "Please enter your lastName properly.");
-        return false;
+    if (!lastName.checkValidity()) {
+        document.getElementById("lastNameError").innerHTML = "Εισάγετε το επίθετο σας.";
+    } else {
+        document.getElementById("lastNameError").innerHTML = "";
     }
 
     // email
+    if (!email.checkValidity()) {
+        document.getElementById("emailError").innerHTML = "Εισάγετε το email σας.";
+    } else {
+        document.getElementById("emailError").innerHTML = "";
+    }
 
     // phone
+    if (!tel.checkValidity()) {
+        document.getElementById("telError").innerHTML = "Εισάγετε το τηλέφωνο σας.";
+    } else {
+        document.getElementById("telError").innerHTML = "";
+    }
 
     // bDay
+    if (!bDay.checkValidity()) {
+        document.getElementById("bDayError").innerHTML = "Εισάγετε την ημερομηνία γεννήσεως σας.";
+    } else {
+        document.getElementById("bDayError").innerHTML = "";
+    }
 
     // // Address
     // region
+    if (!region.checkValidity()) {
+        document.getElementById("regionError").innerHTML = "Εισάγετε την περιφέρειά σας.";
+    } else {
+        document.getElementById("regionError").innerHTML = "";
+    }
 
     // county 
+    if (!county.checkValidity()) {
+        document.getElementById("countyError").innerHTML = "Εισάγετε τον νομό σας.";
+    } else {
+        document.getElementById("countyError").innerHTML = "";
+    }
 
     // postalCode 
+    if (!postalCode.checkValidity()) {
+        document.getElementById("postalCodeError").innerHTML = "Εισάγετε τον ταχυδρομικό σας κώδικα.";
+    } else {
+        document.getElementById("postalCodeError").innerHTML = "";
+    }
 
     // address 
+    if (!address.checkValidity()) {
+        document.getElementById("addressError").innerHTML = "Εισάγετε την διεύθυνση σας.";
+    } else {
+        document.getElementById("addressError").innerHTML = "";
+    }
 
     // // Payment
     // ccNumber 
@@ -86,12 +106,31 @@ function validate() {
 
     // // Account
     // username 
+    if (!username.checkValidity()) {
+        document.getElementById("usernameError").innerHTML = "Εισάγετε το όνομα χρήστη σας.";
+    } else {
+        document.getElementById("usernameError").innerHTML = "";
+    }
 
     // password.length 
+    if (!password.checkValidity()) {
+        document.getElementById("passwordError").innerHTML = "Εισάγετε τον κωδικό σας.";
+    } else {
+        document.getElementById("passwordError").innerHTML = "";
+    }
 
     // password != password1
+    if (!password1.checkValidity()) {
+        document.getElementById("passwordsError").innerHTML = "Ελέγξτε τον κωδικό σας!";
+    } else {
+        document.getElementById("passwordsError").innerHTML = "";
+    }
 
     // // Approves
     // approveTerms
-    return true;
+    if (!approveTerms.checkValidity()) {
+        document.getElementById("approveTermsError").innerHTML = "Η δημιουργία λογαριασμού είναι αδύνατη δίχως την αποδοχή των όρων και προϋποθέσεων.";
+    } else {
+        document.getElementById("approveTermsError").innerHTML = "";
+    }
 }
